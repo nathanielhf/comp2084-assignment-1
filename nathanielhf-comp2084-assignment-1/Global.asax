e66bfcecd173b6aaf845e497604be7a42fab07f0
Nathaniel Fisher <nathaniel.fisher@mygeorgian.ca>
2018-10-08 12:39:20 -0400

    first commit. Includes boilerplate ASP.NET MVC project

---
+++ nathanielhf-comp2084-assignment-1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="nathanielhf_comp2084_assignment_1.MvcApplication" Language="C#" %>
---
+++ nathanielhf-comp2084-assignment-1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(nathanielhf_comp2084_assignment_1.Startup))]
+namespace nathanielhf_comp2084_assignment_1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
